fbf9a726243eef73a1237ead1addb4c9e5bda6cb
JoseAugustoFS <134980478+JoseAugustoFS@users.noreply.github.com>
2023-06-13 22:22:47 -0300

    Primeiro Commit.

---
+++ LojaE-Book/src/main/java/br/edu/iff/bsi/LojaEBook/LojaEBookApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.bsi.LojaEBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaEBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaEBookApplication.class, args);
+	}
+
+}
---
+++ LojaE-Book/src/test/java/br/edu/iff/bsi/LojaEBook/LojaEBookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.bsi.LojaEBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaEBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
