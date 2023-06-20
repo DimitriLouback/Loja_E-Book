755090573bb92c1bbde36cbeeb39eb50ef736dba
JoseAugustoFS <134980478+JoseAugustoFS@users.noreply.github.com>
2023-06-20 08:58:31 -0300

    Primeira página.

---
+++ LojaE-Book/src/main/java/br/edu/iff/bsi/LojaEBook/controller/view/MainControllerView.java
@@ -0,0 +1,15 @@
+package br.edu.iff.bsi.LojaEBook.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/home")
+public class MainControllerView {
+
+	@GetMapping()
+	public String page() {
+		return "index";
+	}
+}
